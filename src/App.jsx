d14dbccc1b1e4410d16e2ec95ba97e1dbfa69c87
palak porwal <porwalpalak10@gmail.com>
2024-07-25 11:28:10 +0530

    sign in/sign up && Login/Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Body from './components/Body'
 function App() {
   
   return (
-    <div className='font-bold'>
-      hello palak
-    </div>
+    <Body/>
     
   )
 }
